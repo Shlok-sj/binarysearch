e4bcc53ceb957cc4ea03ed9034ba3a2b86b02de2
Shlok Sanjay Gang <101562803+Shlok-sj@users.noreply.github.com>
2023-05-24 00:02:14 +0530

    Update bin_ser.cpp

--- bin_ser.cpp
+++ bin_ser.cpp
@@ -9,7 +9,7 @@ int main()
     cout <<"Enter no of elements : ";
     cin>>n;
     
-    cout <<"Enter elements : ";
+    cout <<"Enter elements : \n";
     
     for(int i=0; i<n; i++){
         cout<<i+1<<") ";
